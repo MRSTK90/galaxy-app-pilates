76e3e30e909af90305af237235a50bd4a350a952
kst <kst@osstem.com>
2023-10-19 09:20:41 +0900

    init

---
+++ src/main/java/kr/co/galaxy/galaxyapppilates/GalaxyAppPilatesApplication.java
@@ -0,0 +1,13 @@
+package kr.co.galaxy.galaxyapppilates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyAppPilatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxyAppPilatesApplication.class, args);
+    }
+
+}
