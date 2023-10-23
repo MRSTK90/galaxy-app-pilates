f2516373b6c546b7dab48387d90abc77dc3a08ed
kst <kst@osstem.com>
2023-10-23 19:57:40 +0900

    new : 신체 관련 클래스 생성

---
+++ src/main/java/kr/co/galaxy/app/pilates/domain/Body.java
@@ -0,0 +1,10 @@
+package kr.co.galaxy.app.pilates.domain;
+
+public class Body {
+    private String partId;
+    private String partName;
+
+    private String featureId;
+    private String featureName;
+
+}
